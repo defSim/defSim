5d9bb1dbeddb8317a47be25239f27b37897a33fa
marijnkeijzer <m.a.keijzer@rug.nl>
2019-09-22 18:13:35 +0200

    correct regionscount method

--- defSim/tools/OutputMeasures.py
+++ defSim/tools/OutputMeasures.py
@@ -44,16 +44,19 @@ def clustercount(network):  # returns
     # return(len([len(c) for c in sorted(nx.connected_components(Gsub), key=len, reverse=True)]))
 
 
-def regionscount(network):  # returns a list with the size of all the clusters in the graph
+def regionscount(network, regions_threshold: float = 0):
     """
-    Counts how many agents belong to each cluster in the network and returns a list of these numbers.
+    Counts the number of *cultural regions* present in the graph. Following Axelrod (1997), cultural regions are
+    defined as a set of connected nodes with an identical cultural profile.
 
     :param network: The network to be measured.
+    :param float=0 regions_threshold: Threshold :math:`\in [0,1]` that defines the maximal allowed dissimilarity between
+    two agents to belong to the same region.
     :returns: A list with the size of all the clusters in the graph.
     """
-    Gsub = network.copy()
-    remove = [pair for pair, dissimilarity in nx.get_edge_attributes(Gsub, 'dist').items() if dissimilarity != 0]
-    Gsub.remove_edges_from(remove)
-    # changed the return for single run analysis!!!!
-    # return([len(c) for c in sorted(nx.connected_components(Gsub), key=len, reverse=True)])
-    return (len([len(c) for c in sorted(nx.connected_components(Gsub), key=len, reverse=True)]))
+    networkcopy = network.copy()
+    remove = [pair for pair, dissimilarity in nx.get_edge_attributes(networkcopy, 'dist').items()
+              if dissimilarity > regions_threshold]
+    networkcopy.remove_edges_from(remove)
+
+    return len([len(c) for c in sorted(nx.connected_components(networkcopy), key=len, reverse=True)])
