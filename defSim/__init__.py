6e2f2514654b15e02052eb139d7e28b0d7a7ee51
marijnkeijzer <m.a.keijzer@rug.nl>
2019-10-21 19:07:58 +0200

    make 'select_focal_agent' directly callable

--- defSim/__init__.py
+++ defSim/__init__.py
@@ -8,18 +8,27 @@ __all__ = ["agents_init",
 
 from defSim.agents_init import agents_init
 from defSim.agents_init.agents_init import initialize_attributes
+
 from defSim.network_evolution_sim.network_evolution_sim import NetworkModifier
+
 from defSim.dissimilarity_component import dissimilarity_calculator
 from defSim.dissimilarity_component.HammingDistance import HammingDistance
 from defSim.dissimilarity_component.EuclideanDistance import EuclideanDistance
+
 from defSim.focal_agent_sim import focal_agent_sim
+from defSim.focal_agent_sim.focal_agent_sim import select_focal_agent
+
+from defSim.neighbor_selector_sim import neighbor_selector_sim
+from defSim.network_init.network_init import generate_network
+
 from defSim.influence_sim import influence_sim
 from defSim.influence_sim.influence_sim import spread_influence
 from defSim.influence_sim.influence_sim import InfluenceOperator
-from defSim.neighbor_selector_sim import neighbor_selector_sim
-from defSim.network_init.network_init import generate_network
+
 import defSim.tools
 from defSim.tools import OutputMeasures
 from defSim.tools.CreateOutputTable import create_output_table
+
 from defSim.Experiment import Experiment
+
 from defSim.Simulation import Simulation
