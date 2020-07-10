8f52e13cb3e63187f2f2211cf860c82e5b9877f3
dimaba <diekobakker@hotmail.com>
2020-07-10 15:46:01 +0200

    Add ability to rewire exact number of edges to MaslovSneppenRewiring

--- defSim/network_evolution_sim/MaslovSneppen.py
+++ defSim/network_evolution_sim/MaslovSneppen.py
@@ -3,10 +3,10 @@ import networkx as nx
 from .network_evolution_sim import NetworkModifier
 
 
-class MaslovSneppen(NetworkModifier):
+class MaslovSneppenRewiring(NetworkModifier):
 
     @staticmethod
-    def rewire_network(network: nx.Graph, rewiring_prop: float, **kwargs):
+    def rewire_network(network: nx.Graph, rewiring_prop: float = None, rewiring_exact = None, **kwargs):
         """
         This method executes Maslov Sneppen rewiring (Maslov & Sneppen, 2002). Until a given proportion of the network
         edges is rewired, the algorithm will pick two edges at random, remove them, and construct an edge between a
@@ -18,18 +18,27 @@ class MaslovSneppen(NetworkModifier):
         :param network: NetworkX Graph object
         :param rewiring_prop: A threshold for the minimum proportion of edges in the graph object that need to be rewired.
             Sampling these edges happens with replacement, so rewiring_prop may exceed 1.
+        :param rewiring_exact: The exact number of edges to rewire. One of rewiring_prop or rewiring_exact must be set.
+            If both are set, rewiring_exact takes precedence.
         :return: Does not return the network. The network is modified in place.
         """
+
+        if self.rewiring_exact is not None and self.rewiring_prop is None:
+            rewire_n = self.rewiring_exact
+        elif self.rewiring_exact is None and self.rewiring_prop is not None:
+            rewire_n = self.rewiring_prop * network.number_of_edges()
+        elif self.rewiring_exact is not None and self.rewiring_prop is not None:
+            rewire_n = self.rewiring_exact
+            warnings.warn("Both proportional and exact rewiring specified, using exact.")
+                
         ticker = 0
-        while rewiring_prop * network.number_of_edges() > ticker:
+        while rewire_n > ticker:
             agentA, agentB = random.choice(list(network.edges()))
             agentC, agentD = random.choice(list(network.edges()))
-            if((agentA != agentC) & (agentA != agentD) & (agentB != agentC) & (agentB != agentD) &
-               (network.has_edge(agentA,agentC)==False) & (network.has_edge(agentB,agentD)==False)):
+            if((agentA != agentC) and (agentA != agentD) and (agentB != agentC) and (agentB != agentD) and not
+               network.has_edge(agentA,agentC) and not network.has_edge(agentB,agentD)):
                 network.remove_edge(agentA,agentB)
                 network.remove_edge(agentC,agentD)
                 network.add_edge(agentA,agentC)
                 network.add_edge(agentB,agentD)
                 ticker += 1
-        
-
