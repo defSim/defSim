b27062f684c0cb4008df58de63f336abbbf0acee
dimaba <diekobakker@hotmail.com>
2020-07-14 20:33:52 +0200

    remove irrelevant test files from repo

--- defSim/tests/batchscripts/batch_0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=simulation_chunk_0.job
-#SBATCH --output=output/chunk0.txt
-#SBATCH --time=30:00
-#SBATCH --mem=8000
-#SBATCH --partition=short
-#SBATCH --cpus-per-task=24
-python /home/dieko/VENVs/defSim/lib/python3.6/site-packages/defSim/tools/ClusterExecutionScript.py 0 /home/dieko/Documents/defSim/defSim/tests/pickles /home/dieko/Documents/defSim/defSim/tests/output
